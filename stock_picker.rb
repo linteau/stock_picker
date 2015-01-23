55aba02d81717ddfcd92e9175a2a443214280cbb
andylinteau <andy.linteau@gmail.com>
2015-01-23 03:41:28 -0500

    complete stock picker function

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,57 +1,39 @@
 def stock_picker(array)
 
+	buy_day = 0
+	sell_day = 0
+	bought_day = 0
+	sold_day = 0
+	best_buy_and_sell_date = []
 	diff = 0
-
 	profit = 0
-
-	sorted_profits = []
-
-	if array.max == array[0]
-		array.shift
-	end
-
-	if array.min == array[-1]
-		array.pop
-	end
-
-	puts "array size starting is #{array.size}"
+	sorted_profits = [0]
 
 	(array.size - 1).times do |x|
-
-		puts "this is the x #{x}"
-
-		if x == 0
-
-			(array.size - (x+1)).times do |i|
-				puts "this is the i  #{i}"
-
-				puts diff = array[-1] - array[-2 - i]
-			
-			end
-
-		else
-
+		
 			(array.size - 1).times do |i|
-			puts "this is the i  #{i}"
 
-			puts diff = array[-1] - array[-2 - i]
+				buy_day = array[-2 - i]
+				sell_day = array[-1]
+				diff = array[-1] - array[-2 - i]
 
+					if diff > sorted_profits[-1]
 
+						sorted_profits = sorted_profits.push(diff)
+						bought_day = buy_day
+						sold_day = sell_day
+						best_buy_and_sell_date = best_buy_and_sell_date.push(array.index(buy_day))
+						best_buy_and_sell_date = best_buy_and_sell_date.push(array.index(sell_day))
+					end
 			end
-
-			sorted_profits = sorted_profits.push(diff)
-
-		end
-
-		puts "array has been #{array.pop} popped"
-		puts array
-
-		puts "array size is #{array.size}"
 	end
+	puts best_buy_and_sell_date[-2..-1]
+	puts "It's best to buy on day #{best_buy_and_sell_date[-2]} and best to sell on #{best_buy_and_sell_date[-1]}
+				to get a profit of #{sorted_profits[-1]}"
 
-	puts sorted_profits.sort.reverse
+	return  best_buy_and_sell_date[-2..-1]
 
 end
 
 
-stock_picker([17,3,6,9,15,8,6,1,10])
+stock_picker([17,6,3,9,15,8,6,1,10,5,16])
